5b6db35ee67a0c8cc20332cc5b49cb2a4ed3d326
Kaustubh Khilari <kvstubh@gmail.com>
2025-04-05 14:23:28 -0600

    Initial commit of GrandPrixPredictor project

---
+++ src/main/java/com/example/grandprixpredictor/GrandPrixPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.example.grandprixpredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandPrixPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandPrixPredictorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grandprixpredictor/GrandPrixPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grandprixpredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandPrixPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
